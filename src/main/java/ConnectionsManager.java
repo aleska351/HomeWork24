50c1bf100c1f11b07a84eee5dfdb49dcbd3f06dc
Alena Goncharenko <alonka.goncharenko1994@gmail.com>
2020-05-30 14:34:14 +0300

    Java Elementary

--- src/main/java/ConnectionsManager.java
+++ src/main/java/ConnectionsManager.java
@@ -1,14 +1,26 @@
 public class ConnectionsManager {
-    private static volatile ConnectionsManager instance;
+    private static ConnectionsManager instance;
+    private static final Object lock = new Object();
+
 
     private ConnectionsManager() {
     }
 
     public static ConnectionsManager getInstance() {
         if (instance == null) {
-            instance = new ConnectionsManager();
+            synchronized (lock) {
+                if (instance == null) {
+                    instance = new ConnectionsManager();
+                }
+                try {
+
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.getMessage();
+                }
+
+            }
         }
         return instance;
     }
 }
-
